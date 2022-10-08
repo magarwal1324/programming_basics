4f534a96692451d29a4a2e16e36b3c9ddb8d8401
magarwal1324 <mysticalmegha@proton.me>
2022-10-08 16:28:23 +0530

    check for odd even number

--- lib/programming_exercises/even_odd.ex
+++ lib/programming_exercises/even_odd.ex
@@ -1,12 +1,16 @@
 defmodule EvenOdd do
-  def check_number(x) when rem(x,2) == 0 do
+  def check(x) when is_integer(x) do
+    check_number(x)
+  end
+  def check(_) do
+    "Invalid Number"
+  end
+  defp check_number(x) when rem(x,2) == 0 do
     "Number is Even"
   end
 
-  def check_number(x) when rem(x,2) != 0 do
+  defp check_number(x) when rem(x,2) != 0 do
     "Number is Odd"
   end
-  def check_number(_) do
-    "Invalid Number"
-  end
+
 end
