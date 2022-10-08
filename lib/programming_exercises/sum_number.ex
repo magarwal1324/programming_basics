50df73bc0fe4c71d4b2ce811878dce3e0443d07e
magarwal1324 <mysticalmegha@proton.me>
2022-10-08 16:41:43 +0530

    sum of the number between given range

--- lib/programming_exercises/sum_number.ex
+++ lib/programming_exercises/sum_number.ex
@@ -1,6 +1,11 @@
 defmodule SumNumber do
-  def sum_num(x) when x>5 do
-    x
-    x=x+1
+  @moduledoc """
+  first print the number between x and y then sum of these numbers
+  """
+  def sum_num(x,y) when is_integer(x) and is_integer(y) do
+    Enum.sum(x..y)
+  end
+  def sum_num(_,_) do
+    "Invalid Inputs"
   end
 end
